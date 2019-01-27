8f95f32c73feaf8219a841a5eb8e62351d167101
Abysmo <eterniumsoul@gmail.com>
2019-01-28 00:13:27 +0200

    comments

--- get_my_ip.c
+++ get_my_ip.c
@@ -9,64 +9,64 @@ infoflag ---> if (infoflag == true) - getmyip() print info about local ip, broad
 const char * getmyip(int retflag, int infoflag)
 {
 
-	/*GET DEFAULT interface*/
+    /*GET DEFAULT interface*/
 
-	FILE *f;
-	char line[100] , *p , *c;
+    FILE *f;
+    char line[100] , *p , *c;
 
     f = fopen(ROUTE_PATH , "r");
-	if (f == NULL)
-	{
-		printf(ROUTE_PATH);
-		fprintf(stderr," - Fail to open : %s \n", strerror(errno));
-		printf("Enter your IP in format XXX.XXX.XXX.XXX \n");
-		char stdin_ip[IPV4_ADDR_LEN]={0};
-		scanf("%s", stdin_ip);
-		
+    if (f == NULL)
+    {
+        printf(ROUTE_PATH);
+        fprintf(stderr," - Fail to open : %s \n", strerror(errno));
+        printf("Enter your IP in format XXX.XXX.XXX.XXX \n");
+        char stdin_ip[IPV4_ADDR_LEN]={0};
+        scanf("%s", stdin_ip);
+
         sprintf(alt_local_ip,"\"%s\"",stdin_ip); //concat. with quotation marks
-		
-		
-		int ip_sep = 4;
-		char ip_chk[IPV4_ADDR_LEN + 2];
-		memcpy (ip_chk, alt_local_ip, strlen(alt_local_ip));
-		if ((strtok(ip_chk, ".")) == NULL)
-		{
-			fprintf(stderr,"IP format is wrong ! \n");
-			return NULL;
-		}
-		else
-		{
-			ip_sep--;
-			while ((strtok(NULL,".")) != NULL)
-			ip_sep--;
-			if (ip_sep !=0)
-			{
-				fprintf(stderr,"IP format is wrong !\n");
-				return NULL;
-			}
-		}
-		
-		printf("%s\n", alt_local_ip);
-		printf("OK! \n");
-		return alt_local_ip; 
-	}
+
+
+        int ip_sep = 4;
+        char ip_chk[IPV4_ADDR_LEN + 2];
+        memcpy (ip_chk, alt_local_ip, strlen(alt_local_ip));
+        if ((strtok(ip_chk, ".")) == NULL)
+        {
+            fprintf(stderr,"IP format is wrong ! \n");
+            return NULL;
+        }
+        else
+        {
+            ip_sep--;
+            while ((strtok(NULL,".")) != NULL)
+            ip_sep--;
+            if (ip_sep !=0)
+            {
+                fprintf(stderr,"IP format is wrong !\n");
+                return NULL;
+            }
+        }
+
+        printf("%s\n", alt_local_ip);
+        printf("OK! \n");
+        return alt_local_ip;
+    }
 
     //parsing ROUTE_PATH("/proc/net/route") for default interface
-	while(fgets(line , 100 , f))
-	{
-		p = strtok(line , " \t");
-		c = strtok(NULL , " \t");
-	
-		if(p!=NULL && c!=NULL)
-		{
-			if(strcmp(c , "00000000") == 0)
-			{
-				break;
-			}
-		}
-	}
+    while(fgets(line , 100 , f))
+    {
+        p = strtok(line , " \t");
+        c = strtok(NULL , " \t");
 
-	/*get local IP*/
+        if(p!=NULL && c!=NULL)
+        {
+            if(strcmp(c , "00000000") == 0)
+            {
+                break;
+            }
+        }
+    }
+
+    /*get local IP*/
 
 	int fd;
 	struct ifreq ifr;
@@ -77,12 +77,12 @@ const char * getmyip(int retflag, int infoflag)
         fprintf(stderr,"Creating temp socket error : %s \n", strerror(errno));
         return NULL;
     }
-	/*get an IPv4 IP address */
-	ifr.ifr_addr.sa_family = AF_INET;
+    /*get an IPv4 address */
 
-	/* IP address attached to default interface */
-	strncpy(ifr.ifr_name, p, IFNAMSIZ-1);
-
+    //set socket type
+    ifr.ifr_addr.sa_family = AF_INET;
+    //set socket default interface
+    strncpy(ifr.ifr_name, p, IFNAMSIZ-1);
 /*
         SIOCGIFADDR
         Get  or set the address of the device using ifr_addr.  Setting
@@ -90,20 +90,19 @@ const char * getmyip(int retflag, int infoflag)
         bility, only AF_INET addresses are accepted or returned.
  */
 	ioctl(fd, SIOCGIFADDR, &ifr);
-
 	close(fd);
 
 	/* making broadcast ip */
 
     strcpy (local_ip, inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
-	strncpy (broadcast_ip, local_ip, (sizeof(broadcast_ip)*(sizeof(char))));		
+    strncpy (broadcast_ip, local_ip, strlen(local_ip));
 
  	char * lastByte = strrchr(broadcast_ip, '.');
 	memcpy(++lastByte, "255", (3*sizeof(char)));
 	
 	if(infoflag)
 	{
-		printf("Default interface is : %s \n" , p);
+        printf("Default interface is : %s \n" , p);
 		printf("local IP : %s\n", local_ip);
 		printf("broadcast IP : %s\n", broadcast_ip);
 	}
